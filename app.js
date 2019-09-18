00c9028c75cec95af0e92adfc3d35333e61b29cb
Yue Zuo <woshizuoyue9201@yahoo.com>
2019-09-18 14:25:52 -0400

    UI controller

--- app.js
+++ app.js
@@ -8,20 +8,57 @@ var BudgetController = (function(){
 
 var UIController = (function(){
 
+    var DomStirng = {
+
+        inputType : '.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+    };
+
+    var operationObj = {
+
+        getInput : function(){
+
+            return{
+
+                type : document.querySelector(DomStirng.inputType).value,
+                description : document.querySelector(DomStirng.inputDescription).value,
+                value : document.querySelector(DomStirng.inputValue).value
+            }
+        },
+
+        getDomString : function(){
+
+            return DomStirng;
+
+        }
+
+    }
+
+    return operationObj;
+
 })();
 
 var controller = (function(budgetCtrl, UICtrl){
 
+    var Dom = UICtrl.getDomString();
     var ctrlAddItem = function(){
 
         //1. get the field input data;
+
+        var input = UICtrl.getInput();
+
+        console.log(input);
         //2. add the item to the budget controller;
         //3. add the item to the UI;
         //4. calculate the budget;
         //5. display the budget on the UI;
+
+
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(Dom.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress',function(event){
         
