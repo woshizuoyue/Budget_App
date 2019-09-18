3a2af80fe2c0f5bc537e33171f4f5c8f2264c5d6
Yue Zuo <woshizuoyue9201@yahoo.com>
2019-09-18 11:19:19 -0400

    edit controller

--- app.js
+++ app.js
@@ -2,34 +2,7 @@
 
 var BudgetController = (function(){
 
-    var x = 23;
-
-    var add = function(a){
-
-        return a + x;
-    }
-
-    // same as add() method. two ways to define function;
-    // 1. function _name (...){};
-    // 2. var x = function(...){};
     
-    function minus (a){
-
-        return x - a;
-    }
-
-    return{
-
-        publicTest : function(b){
-
-            return add(b);
-        },
-
-        publicMinusTest: function(b){
-
-            return minus(b);
-        }
-    }
 
 })();
 
@@ -39,20 +12,26 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function(){
 
-    var y = budgetCtrl.publicMinusTest(5);
-
-    return{
-
-        anotherPublic: function(){
-            
-            console.log(z);
-        },
-
-        anotherMinusPublic : function(){
-
-            console.log(y);
-        }
+        //1. get the field input data;
+        //2. add the item to the budget controller;
+        //3. add the item to the UI;
+        //4. calculate the budget;
+        //5. display the budget on the UI;
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress',function(event){
+        
+        if(event.keyCode === 13 || event.which === 13){
+
+            ctrlAddItem();
+        }
+    
+       
+    });
+
+    
 })(BudgetController, UIController);
